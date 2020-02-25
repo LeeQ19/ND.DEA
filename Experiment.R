1c304f1d3672dc234edc45ba752a5c24d6e2a0a1
LeeQ19 <leekw1994@gmail.com>
2020-02-25 13:08:56 +0900

    Add code to remove outlier

--- Experiment.R
+++ Experiment.R
@@ -10,6 +10,7 @@ df.f.2d <- read.csv(url("https://docs.google.com/spreadsheets/d/e/2PACX-1vSaNq2L
 df.f.3d <- simplify2array(by(df.f.2d[, -c(1)], df.f.2d$YEAR, as.matrix))
 
 # Set parameter
+id.out <- c(2)
 id.t <- c(1)
 id.x1 <- c(2, 13)
 id.x2 <- c(4, 11)
@@ -22,11 +23,11 @@ orientation  <- "i"
 wv <- NULL
 engine <- "Ipopt"
 
+df.f.3d <- df.f.3d[-id.out, , ]
+
 # Measure efficiency
 # Engine = c("ANTIGONE", "BARON", "CONOPT", "Ipopt", "Knitro", "LINDOGlobal", "MINOS", "PATHNLP", "SBB", "SNOPT")
 res.nd.nlp <- nd.dea.nlp(df.f.3d[, id.x1, ], df.f.3d[, id.x2, ], df.f.3d[, id.y1, ], df.f.3d[, id.y2, ], df.f.3d[, id.z, ], df.f.3d[, id.zl, ], engine = engine)
 # load("res.nlp.Rdata")
 
 write.csv(res.nd.nlp, file = paste0(engine, ".csv"))
-write.csv(res.nd.nlp$z21data, file = "test21.Ipopt.csv")
-write.csv(res.nd.nlp$z22data, file = "test22.Ipopt.csv")
